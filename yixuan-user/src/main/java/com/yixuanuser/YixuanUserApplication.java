69edb487ecc7902f2f9a6ef0c9c0032492e518f6
yixuan <tangyixuanemail@163.com>
2023-01-30 21:08:54 +0800

    初始化

---
+++ yixuan-user/src/main/java/com/yixuanuser/YixuanUserApplication.java
@@ -0,0 +1,13 @@
+package com.yixuanuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YixuanUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YixuanUserApplication.class, args);
+    }
+
+}
---
+++ yixuan-user/src/test/java/com/yixuanuser/YixuanUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yixuanuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YixuanUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
